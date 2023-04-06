48e93ea2980e2c32fff0ea04aa4815a5f25257f8
emmanuelnwogbo <your.email@example.com>
2023-04-06 01:43:25 +0100

    add get terms route

---
+++ server/get_terms.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Term = require('./models/term');
+
+router.get('/terms', async (req, res) => {
+    try {
+        Term.find()
+            .then((terms) => {
+                console.log('All the terms:', terms);
+                res.status(200).send({
+                    terms
+                })
+            })
+            .catch((err) => {
+                console.error('Error getting the terms', err);
+            });
+    } catch {
+
+    }
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 
 const post_term = require('./post_term');
+const get_terms = require('./get_terms');
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', post_term);
+app.use('/api', get_terms);
 
 app.listen(3000, () => {
     mongooseDB.connect('mongodb+srv://sampelumibiz:uRRpeKcqAbi8rrTd@cluster0.ot6j04n.mongodb.net/?retryWrites=true&w=majority', {
